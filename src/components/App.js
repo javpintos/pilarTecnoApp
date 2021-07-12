e2cfb0230232feacdbe7a0afaed0147c3383b972
Javier Pintos <jpintos234@gmail.com>
2021-07-12 16:26:05 -0300

    buttons added

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,102 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  Dimensions,
-  StatusBar,
-  StyleSheet,
-  Text,
-  ImageBackground,
-  useColorScheme,
-  View,
-} from 'react-native';
-import Home from '../screens/Home'
+    SafeAreaView,
+    ScrollView,
+    Dimensions,
+    StatusBar,
+    StyleSheet,
+    Text,
+    ImageBackground,
+    useColorScheme,
+    View,
+  } from 'react-native';
 
-const height = Dimensions.get('window').height
-const width = Dimensions.get('window').width
+  const height = Dimensions.get('window').height
+  const width = Dimensions.get('window').width
 
+  export default class Home extends React.Component {
+    render(){
+        return(
+        <SafeAreaView>
+          <ImageBackground
+            style={{height, justifyContent:'center', alignItems:'center', flexDirection:'column'}}
+            imageStyle={{}}
+            source={require('../assets/images/fondo7.jpg')}
+          >
+          <View style={{flexDirection:'row'}}>
 
- const App = () => {
+            {/* <View style={[styles.button, {backgroundColor:'green'}]} */}
+            <View style={{}}>
+              <ImageBackground
+                style={[styles.button, {backgroundColor:'transparent'}]}
+                imageStyle={{borderRadius:15}}
+                source={require('../assets/patterns/pattern1.jpg')}
+              >
+                <Text style={styles.text}>
+                  Inicio
+                </Text>
+              </ImageBackground>
+            </View>
 
-    return( 
-        <Home />
-    )
-}
+            <View style={{}}>
+              <ImageBackground
+                style={[styles.button, {backgroundColor:'transparent'}]}
+                imageStyle={{borderRadius:15}}
+                source={require('../assets/patterns/pattern2.jpg')}
+              >
+                <Text style={styles.text}>
+                  Nosotros
+                </Text>
+              </ImageBackground>
+            </View>
+          </View>
 
-const styles = StyleSheet.create({
-  text: {
-    fontSize:30, 
-    fontWeight:'bold', 
-    color:'red',
-    textAlign:'center'
-  },
-  button: {
-    margin: width/20,
-    height:width/2.5,
-    width:width/2.5,
-    borderRadius:15,
-    justifyContent:'center',
-    backgroundColor:'#fff',
-    zIndex:1
+          <View style={{flexDirection:'row'}}>
+            
+          <View style={{}}>
+              <ImageBackground
+                style={[styles.button, {backgroundColor:'transparent'}]}
+                imageStyle={{borderRadius:15}}
+                source={require('../assets/patterns/pattern3.jpg')}
+              >
+                <Text style={styles.text}>
+                  Servicios
+                </Text>
+              </ImageBackground>
+            </View>
+
+            <View style={{}}>
+              <ImageBackground
+                style={[styles.button, {backgroundColor:'transparent'}]}
+                imageStyle={{borderRadius:15}}
+                source={require('../assets/patterns/pattern4.jpg')}
+              >
+                <Text style={styles.text}>
+                  Contacto
+                </Text>
+              </ImageBackground>
+            </View>
+          </View>
+          
+          </ImageBackground>        
+        </SafeAreaView>
+        )}
   }
-})
 
-export default App;
+  const styles = StyleSheet.create({
+    text: {
+      fontSize:30,
+      fontWeight:'bold',
+      color:'red',
+      textAlign:'center'
+    },
+    button: {
+      margin: width/20,
+      height:width/2.5,
+      width:width/2.5,
+      borderRadius:15,
+      justifyContent:'center',
+      backgroundColor:'#fff',
+      zIndex:1
+    }
+  })
