27b8eedea3c0a04d995499f75b6c5ef382208905
Javier Pintos <jpintos234@gmail.com>
2021-07-15 01:51:39 -0300

    task 1 finished

--- android/app/src/main/java/com/proyecto2/MainActivity.java
+++ android/app/src/main/java/com/proyecto2/MainActivity.java
@@ -1,4 +1,4 @@
-package com.proyecto2;
+package com.pilartectoapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "proyecto2";
+    return "pilarTectoApp";
   }
 }
